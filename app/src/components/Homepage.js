f1307562cc3bfb86c4b91ac24bc6c246b9750645
Carmen <xiawenwu22@gmail.com>
2020-05-13 01:13:57 -0400

    add empty Homepage component for default route

---
+++ app/src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+
+export default class Homepage extends Component {
+  constructor() {
+    super();
+ }
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    );
+  }
+}
