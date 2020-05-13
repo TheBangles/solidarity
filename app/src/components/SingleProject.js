58f946e62492aa6f940d332fd94e0a6363a2eced
tiff <wong.tiffanyh@gmail.com>
2020-05-13 17:47:14 -0400

    add state to SingleProject

--- app/src/components/SingleProject.js
+++ app/src/components/SingleProject.js
@@ -3,24 +3,40 @@ import React, { Component } from 'react';
 export default class SingleProject extends Component {
   constructor(props) {
     super();
+    this.state = {
+      singleProject: undefined,
+    };
   }
 
   async componentDidMount() {
-    // const singleProject = await this.props.drizzle.contracts.Donate.methods
-    //   .readSingleProject(4)
-    //   .call();
-    // console.log('singlee', singleProject);
+    const singleProject = await this.props.drizzle.contracts.Donate.methods
+      .readSingleProject(4)
+      .call();
+    this.setState({
+      singleProject,
+    });
+    console.log('singlee', this.state.singleProject);
   }
 
   render() {
+    // const singleProject = this.state.singleProject;
+    // if (singleProject === 'undefined') {
+    //   return <h1>No project</h1>;
+    // }
+    // return (
+    //   <div>
+    //     <h3>{singleProject[2]}</h3>
+    //     <p>Description: {singleProject[3]}</p>
+    //     <p>Goal: {singleProject[4]}</p>
+    //     <p>Amount Donated: {singleProject[5]}</p>
+    //   </div>
+    // );
     return (
       <div>
-        <h3>Single Component</h3>
-        <p>Placehold projectName</p>
-        <p>Placehold description</p>
-        <p>Placehold amountNeeded - goal</p>
-        <p>Placehold projectName</p>
-        <p>Placehold amountDonated</p>
+        <h3>Name</h3>
+        <p>Description</p>
+        <p>Goal</p>
+        <p>Amount Donated</p>
       </div>
     );
   }
