a0cd5967aa8bc80f08e262c2d6d5212261be1710
Carmen <xiawenwu22@gmail.com>
2020-05-15 01:09:58 -0400

    draft Trending component

---
+++ app/src/components/Trendings.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default class Trendings extends React.Component {
+  constructor(props, context) {
+    super(props);
+    this.drizzleState = context.drizzle;
+    this.state= {
+      projects: []
+    };
+  }
+
+  // getAccount(): for Array only
+  async componentDidMount() {
+    // await this.props.drizzle.web3.eth.
+    // Get total number of projects
+    const length = await this.props.drizzle.contracts.Donate.nextId.call();
+
+    const projects = [];
+    for (let i = 1; i <= length; i++) {
+      let project = await this.props.drizzle.contracts.Donate.methods.readSingleProject(i).call();
+      projects.push(project);
+    }
+
+    this.setState({
+      projects: projects
+    });
+  }
+
+  /*
+  project = [id, recipient, projectName, description, amountNeeded, amountDonated]
+  */
+
+  render() {
+    return(
+      <div>
+        {this.state.projects.map((project) =>
+          <div key={project[0]}>
+            <div>{project[1]}</div>
+            <div>{project[2]}</div>
+            <div>{project[3]}</div>
+            <div>{project[4]}</div>
+            <div>{project[5]}</div>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+}
