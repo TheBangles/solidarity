5a1f0bcd30493a62d5d2c8d0fb303d3c7ca2e9a2
tiff <wong.tiffanyh@gmail.com>
2020-05-19 15:53:42 -0400

    removed obsolete lines from AllProjects

--- app/src/components/AllProjects.js
+++ app/src/components/AllProjects.js
@@ -15,11 +15,6 @@ export default class AllProjects extends Component {
   }
 
   async componentDidMount() {
-    // console.log(this.props)
-    // if (!this.state.userAddress) {
-    //   const accounts = await this.props.drizzle.web3.eth.getAccounts();
-    //   this.setState({ userAddress: accounts[0] });
-    // }
     this.props.drizzle.contracts.Donate.methods.getAllProjectsLength.cacheCall();
   }
 
