8dc2e6e3000d29fa9b56d5ad1d811089b211c951
Jennifer Chen <jenniferchen95@gmail.com>
2020-05-19 11:13:55 -0400

    added metamask instructions

--- app/src/components/MetaMask.js
+++ app/src/components/MetaMask.js
@@ -8,6 +8,11 @@ export default class MetaMask extends Component {
     return (
       <center>
         Please download <a href="https://metamask.io/"> MetaMask </a>  to use Solidarity
+        <hr></hr>
+        <li> Solidarity is a decentralized app that is built on top of the Ethereum blockchain. </li>
+        <li> Donating or creating charity projects require users to submit transactions to the blockchain.</li>
+        <li> MetaMask is available as a  browser extension that serves as a crypto wallet. </li>
+        <li> It allows you to interact with our application by setting up your own accounts</li>
       </center>
     )
   }
