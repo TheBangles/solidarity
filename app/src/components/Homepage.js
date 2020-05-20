8b0f5376b6d50a583fc8f0f4fbb3d7895886ee25
Liang Liang <lilysway@hotmail.com>
2020-05-20 13:01:54 -0400

    title, tagline, and image

--- app/src/components/Homepage.js
+++ app/src/components/Homepage.js
@@ -7,13 +7,46 @@ export default class Homepage extends Component {
 
   render() {
     return (
-      <div class="container">
-        <div class="notification">
-          <span className="title">
-            {/* {' '} */}
-            Solidarity: A combination of Solidity and Charities
-          </span>
-          <span></span>
+      <div>
+        <section id="home" class="hero is-medium">
+          <div class="hero-body">
+            <div class="container">
+              <div class="columns is-vcentered">
+                <div class="column is-6">
+                  <h1 class="title is-title is-size-1 has-text-weight-bold">
+                    Welcome to Solidarity
+                  </h1>
+
+                  <p class="subtitle is-4 has-margin-top-20 is-family-primary">
+                    Donate to Chairties with confidence using SECURE and
+                    TRANSPARENT blockchain network.
+                  </p>
+                </div>
+                <div class="column is-5 has-text-centered">
+                 <img
+                    src="https://www.thepongofund.org/wp-content/uploads/2015/06/Quote-1898205_641455502599124_1991305080_n-FB-6-9-15.jpg"
+                    alt="kids working together to rescue stranded dog out of river"
+                  />
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );}}
+
+
+      {/* // <div class="container">
+      //   <div class="notification"> */}
+
+
+            {/* <span className="title">
+
+            Welcome to Solidarity
+            </span>
+            <span>Donate to Chairties with confidence using SECURE and TRANSPARENT blockchain network.</span>
+
           <div className="text">
             Solidarity is a decentralized platform with a fully transparent
             smart contract on the Ethereum blockchain that allows people to
@@ -26,19 +59,20 @@ export default class Homepage extends Component {
               src="https://www.thepongofund.org/wp-content/uploads/2015/06/Quote-1898205_641455502599124_1991305080_n-FB-6-9-15.jpg"
               alt="kids working together to rescue stranded dog out of river"
             />
-          </div>
+          </div> */}
+
           {/* <div>
             <div className="trending" id="trending">
               Trending Projects
             </div>
             <Trendings {...this.props} />
           </div> */}
-        </div>
-      </div>
+      //   </div>
+      // </div>
 
       //   <div>
       //     <div className='impacts' id='impacts'>Solidarity by the numbers / impacts</div>
       //   </div>
-    );
-  }
-}
+
+
+
