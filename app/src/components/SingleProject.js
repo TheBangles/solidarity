ba7c6aa862f701bfc007a19dadf8fffc5e2eca9e
tiff <wong.tiffanyh@gmail.com>
2020-05-20 15:33:33 -0400

    rerender state after donate

--- app/src/components/SingleProject.js
+++ app/src/components/SingleProject.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import SingleProjectForm from './SingleProjectForm';
-import Loader from 'react-loader-spinner'
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-const convert = require("ether-converter");
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+const convert = require('ether-converter');
 
 export default class SingleProject extends Component {
   constructor(props) {
@@ -32,19 +32,19 @@ export default class SingleProject extends Component {
     });
   };
 
-  // handleSubmit = async (event) => {
-  //   event.preventDefault();
   handleSubmit = async () => {
     let id = this.props.history.location.pathname.slice(8);
     let toast;
 
-    let amountDonated = convert(this.state.amount, "ether").wei;
+    let amountDonated = convert(this.state.amount, 'ether').wei;
 
     try {
       await this.props.drizzle.contracts.Donate.methods.donate(id).send({
         value: amountDonated,
       });
+      const update = this.state.singleProject[5] + amountDonated;
       this.setState({
+        singleProject: { ...this.state.singleProject, [5]: update },
         amount: 0,
       });
       toast = true;
@@ -56,20 +56,19 @@ export default class SingleProject extends Component {
   };
 
   render() {
-
     // let singleProject = this.state.singleProject || 'not mounted';
     if (!this.state.singleProject) {
-      return(
-        <Loader
-           type="ThreeDots"
-           color="#83C5BE"
-           height={100}
-           width={100}
-           timeout={3000} //3 secs
-        />
-       );
-    }
       return (
+        <Loader
+          type="ThreeDots"
+          color="#83C5BE"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
+      );
+    }
+    return (
       <SingleProjectForm
         handleChange={this.handleChange}
         handleSubmit={this.handleSubmit}
