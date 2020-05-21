a50a5932cd763e212c0478e5a0b64e5b91d7ea8d
Carmen <xiawenwu22@gmail.com>
2020-05-20 22:20:42 -0400

    clean up unused template

--- app/src/components/Search.js
+++ app/src/components/Search.js
@@ -1,5 +1,7 @@
 import algoliasearch from 'algoliasearch';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import {
   InstantSearch,
   Hits,
@@ -9,22 +11,19 @@ import {
   ClearRefinements,
   Configure,
 } from 'react-instantsearch-dom';
-import PropTypes from 'prop-types';
-// import '../App.css';
-import { Link } from 'react-router-dom';
 const searchClient = algoliasearch('5QW3O4IWII', '4962981df99de5b545e9fbe9911675bf');
 const index = searchClient.initIndex('project_index');
 
 
-// mount pushed data
 class Search extends Component {
+
+  // mount pushed data
   async componentDidMount(){
     // length = last nextid of projects, in our contract, nextId starts at 1
     const length = await this.props.drizzle.contracts.Donate.methods.nextId().call();
 
     // Get all projects for search
     const projects = [];
-
     for (let i = 1; i < length; i++) {
       let project = await this.props.drizzle.contracts.Donate.methods.readSingleProject(i).call();
       projects.push(project);
@@ -37,25 +36,18 @@ class Search extends Component {
     }).then(({ objectIDs }) => {
       console.log(objectIDs);
     });
-
   }
 
   // built-in demo code
   render() {
     return (
-      <div className="ais-InstantSearch">
-        <InstantSearch indexName="project_index" searchClient={searchClient}>
-          <div className="left-panel">
-            <ClearRefinements />
-            <Configure hitsPerPage={20} />
-          </div>
-          <div className="right-panel">
-            <SearchBox />
-            <Hits hitComponent={Hit} />
-            <Pagination />
-          </div>
-        </InstantSearch>
-      </div>
+      <InstantSearch indexName="project_index" searchClient={searchClient}>
+          <ClearRefinements />
+          <Configure hitsPerPage={20} />
+          <SearchBox />
+          <Hits hitComponent={Hit} />
+          <Pagination />
+      </InstantSearch>
     );
   }
 }
@@ -64,17 +56,13 @@ class Search extends Component {
 function Hit(props) {
   return (
     <div>
-      <img src={props.hit.image} align="left" alt={props.hit.name} />
-      <div className="hit-name">
-        <Highlight attribute="name" hit={props.hit} />
-      </div>
-      <div className="hit-description">
-        <Highlight attribute="description" hit={props.hit} />
-      </div>
+      {/* <img src={props.hit[7]} align="left" alt={props.hit.name} /> */}
+      <Highlight attribute="name" hit={props.hit} />
+      <Highlight attribute="description" hit={props.hit} />
       <div>
-        <Link to={`/single`}>
-          <div> Name {props.hit[2]}</div>
-          <div> imgUrl {props.hit[7]}</div>
+        <Link to={`/single/${props.hit[0]}`}>
+          <div> Name {props.hit[2]} </div>
+          <div> imgUrl {props.hit[7]} </div>
         </Link>
       </div>
     </div>
