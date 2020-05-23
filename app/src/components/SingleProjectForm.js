5a302b1456a77120eab286cda4b612eb6e9373db
tiff <wong.tiffanyh@gmail.com>
2020-05-22 20:22:37 -0400

    increase confetti speed

--- app/src/components/SingleProjectForm.js
+++ app/src/components/SingleProjectForm.js
@@ -25,7 +25,7 @@ const SingleProjectForm = (props) => {
 
   return (
     <div>
-      {donate && <Confetti recycle={false} initialVelocityY={50} />}
+      {donate && <Confetti recycle={false} gravity={0.7} />}
       <div className="content">
         <br></br>
         {/* <div className="notification"> */}
